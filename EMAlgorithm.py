d6400d21d76a6d557e5f666cdd365a72e057324e
BoazB <berman.boaz@gmail.com>
2016-01-16 16:08:25 +0200

    Merge branch 'master' of https://github.com/boazberman/AppliedProbabilityBIUEx4
    # Conflicts:
    #	ex4.py

--- EMAlgorithm.py
+++ EMAlgorithm.py
@@ -1,13 +1,16 @@
+import math
 
 class EMAlgorithm:
 
     def __init__(self, articels, numClusters):
+        self.k = 10
+        self.first = True
         self.numClusters = numClusters
         self.articels = articels
         self.clusters = {}
         self.words = self.initWords()
-        self.documentToCategoryProbability = {}
-        self.wordInClusterProbability = {}
+        self.wti = {}
+        self.pik = {}
         self.pCi = []
         self.initClusters()
 
@@ -20,33 +23,55 @@ class EMAlgorithm:
     def initWords(self):
         words = []
         for article in self.articels:
-            words.append(article.words)
+            words += article.histogram.keys()
         return set(words)
 
     def initDocumentToCategoryProbability(self):
         for article in self.articels:
             for i in xrange(1,self.numClusters,1):
                 if(article in self.clusters[i]):
-                    self.documentToCategoryProbability[(article,i)] = 1
+                    self.wti[(article, i)] = 1
                 else:
-                    self.documentToCategoryProbability[(article,i)] = 0
+                    self.wti[(article, i)] = 0
+
+    def allzi(self):
+        allzi = {}
+        for cluster in self.clusters:
+            allzi[cluster] = math.log(self.pCi[cluster]) + sum([math.log(self.pik[(word, cluster)]) * article.histogram[word] for word in article.histogram.keys() for article in self.articels])
+
+        return allzi;
+
+
+    def calcwti(self, article, cluster, allzi, maxzi):
+        if(allzi[cluster] - maxzi < -1 * self.k):
+            return 0
+        mechane = sum([math.pow(math.e, allzi[subcluster] - maxzi) for subcluster in self.clusters if
+                    allzi[subcluster] - maxzi >= -1 * self.k])
+        return math.pow(math.e, allzi[cluster] - maxzi) / mechane
+
 
     def algorithm(self):
         #E-Step
-        for article in self.articels:
-            for cluster in self.clusters:
-                mona,mechane = 0,0
-
-
+        if(not self.first):
+            self.e_step()
+        else:
+            self.first = False
         #M-Step
         for i in xrange(1,self.numClusters,1):
-            self.pCi[i] = sum([probability for (doc,j),probability in self.documentToCategoryProbability.iteritems() if j==i])
+            self.pCi[i] = sum([probability for (doc, j), probability in self.wti.iteritems() if j == i])
             self.pCi[i] /= len(self.articels)
 
         for word in self.words:
             for cluster in self.clusters:
-                mona,mechane = 0,0
+                mona, mechane = 0.0, 0.0
                 for article in self.articels:
-                    mona += article.histogram[word] * self.documentToCategoryProbability[(article,cluster)]
-                    mechane += article.wordsLen * self.documentToCategoryProbability[(article,cluster)]
-                self.wordInClusterProbability[word,cluster] = mona/mechane
+                    mona += article.histogram[word] * self.wti[(article, cluster)]
+                    mechane += article.wordsLen * self.wti[(article, cluster)]
+                self.pik[(word, cluster)] = mona / mechane
+
+    def e_step(self):
+        allzi = self.allzi()
+        maxzi = max(allzi)
+        for article in self.articels:
+            for cluster in self.clusters:
+                self.wti[(article, cluster)] = self.calcwti(article, cluster, allzi, maxzi)
