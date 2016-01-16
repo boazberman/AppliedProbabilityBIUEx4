0912c97bfee1b0dbe927fb3bb2953c562097d0d5
BoazB <berman.boaz@gmail.com>
2016-01-16 18:12:46 +0200

    Likelihood with maxzi and allzi changes

--- EMAlgorithm.py
+++ EMAlgorithm.py
@@ -1,4 +1,5 @@
 import math
+from itertools import groupby
 
 class EMAlgorithm:
 
@@ -7,7 +8,6 @@ class EMAlgorithm:
         self.epsilon = 0.000001
         self.lamda = 0.0001
         self.vocabolarySize = vocabolarySize
-        self.first = True
         self.numClusters = numClusters
         self.articels = articels
         self.clusters = {}
@@ -41,7 +41,7 @@ class EMAlgorithm:
         allzi = {}
         for cluster in self.clusters:
             for article in self.articels:
-                allzi[(cluster,article)] = math.log(self.pCi[cluster]) + sum([math.log(self.pik[(word, cluster)]) * article.histogram[word] for word in article.histogram.keys()])
+                allzi[(cluster, article)] = math.log(self.pCi[cluster]) + sum([math.log(self.pik[(word, cluster)]) * article.histogram[word] for word in article.histogram.keys()])
 
         return allzi
 
@@ -59,7 +59,7 @@ class EMAlgorithm:
         for i in xrange(1,self.numClusters,1):
             self.pCi[i] /= sum
 
-    def fixPIKptobability(self):
+    def fixPIKProbability(self):
         sum = sum(self.pik)
         for i in xrange(1,self.numClusters,1):
             for word in self.words:
@@ -67,38 +67,65 @@ class EMAlgorithm:
 
 
     def algorithm(self):
-        #E-Step
-        if(not self.first):
-            self.e_step()
-        else:
-            self.first = False
-        #M-Step
-        for i in xrange(1,self.numClusters,1):
+        lastlikelihood = None
+        allzi, maxzi = None, None
+        while True:
+            # E-Step
+            if(lastlikelihood != None):
+                self.e_step(allzilist, maxzi)
+            else:
+                self.first = False
+            # M-Step
+            self.m_step()
+            allzilist, maxzi = self.calc_zi()
+            # Likelihood
+            likelihood = self.likelihood(allzilist, maxzi)
+            if(lastlikelihood != None and likelihood < lastlikelihood):
+                break
+            lastlikelihood = likelihood
+
+
+
+
+    def m_step(self):
+        for i in xrange(1, self.numClusters, 1):
             self.pCi[i] = sum([probability for (doc, j), probability in self.wti.iteritems() if j == i])
             self.pCi[i] /= len(self.articels)
-            if(self.pCi[i] < self.epsilon):
+            if (self.pCi[i] < self.epsilon):
                 self.pCi[i] = self.epsilon
-
-        if(sum(self.pCi) != 1):
+        if (sum(self.pCi) != 1):
             self.fixPCIprobability()
-
         for word in self.words:
             for cluster in self.clusters:
                 mona, mechane = 0.0, 0.0
                 for article in self.articels:
                     mona += article.histogram[word] * self.wti[(article, cluster)]
                     mechane += article.wordsLen * self.wti[(article, cluster)]
-                    if(mona == 0):
+                    if (mona == 0):
                         mona += self.lamda
-                        mechane += self.vocabolarySize*self.lamda
-                    self.pik[word,cluster] = mona / mechane
+                        mechane += self.vocabolarySize * self.lamda
+                    self.pik[word, cluster] = mona / mechane
+        if (sum(self.pik) != 1):
+            self.fixPIKProbability()
 
-        if(sum(self.pik) != 1):
-            self.fixPIKptobability()
-
-    def e_step(self):
-        allzi = self.allzi()
-        maxzi = max(allzi.values())
+    def e_step(self, allzi, maxzi):
+        # Calculate wti through all articles and clusters.
         for article in self.articels:
             for cluster in self.clusters:
-                self.wti[(article, cluster)] = self.calcwti(article, cluster, allzi, maxzi)
+                self.wti[(article, cluster)] = self.calcwti(article, cluster, allzi, maxzi[article])
+
+    def calc_zi(self):
+        allzi = self.allzi()
+        # Calculate max zi for each document through all clusters.
+        maxzi = {}
+        for article, values in groupby(sorted(allzi, key=lambda tup: tup[0][1]), key=lambda key: key[1]):
+            maxzi[article] = max(zivalue for key, zivalue in values)
+        return allzi, maxzi
+
+    def likelihood(self, allzi, maxzi):
+        total = 0.0
+
+        for article in self.articels:
+            total += maxzi[article] + math.log(sum(math.pow(math.e, zi - maxzi[article]) for zi in allzi if zi - maxzi[article] > -1 * self.k))
+
+        return total
